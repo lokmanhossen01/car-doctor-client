a0574e75a09af1065843c26807f2eef3f446e6cc
lokmanhossen01 <https://github.com/lokmanhossen01>
2024-05-09 10:36:53 +0600

    basic setup done with Home, Login, Signup page

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+const Services = () => {
+
+    const [Services, setServices] = useState([]);
+
+    useEffect( () =>  {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
+
+    return(
+        <div className=" mt-4">
+            <div className=" text-center">
+            <h3 className="text-3xl text-orange-600">Our Services</h3>
+            <h2 className="text-5xl"> Our Services Area</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, nisi. Voluptatem repellendus facere adipisci vitae. Sint magni id nihil. Accusantium exercitationem amet, delectus doloribus harum aliquid iusto incidunt dicta saepe!</p>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+                {
+                    Services.map(Service => <ServiceCard key={Service._id} Service={Service}></ServiceCard>)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Services; 
