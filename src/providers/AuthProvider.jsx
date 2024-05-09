a4a7f68390054d9536ee5024f8ddb59565bfcfe5
lokmanhossen01 <https://github.com/lokmanhossen01>
2024-05-09 22:48:28 +0600

    module61 with JWT Token

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
@@ -27,9 +28,29 @@ const AuthProvider = ({children}) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser = {email: userEmail}
             setUser(currentUser)
             console.log('current user', currentUser)
             setLoading(false)
+
+            // if user exists then issue a token
+            if(currentUser) {
+                
+                axios.post('http://localhost:5000/jwt',loggedUser, {withCredentials: true})
+                .then(res => {
+                    console.log('token response',res.data)
+                })
+            }
+
+            else {
+                axios.post('http://localhost:5000/logout', loggedUser, {
+                    withCredentials: true
+                })
+                .then(res => {
+                    console.log(res.data);
+                })
+            }
         });
         return () => {
             return unsubscribe();
